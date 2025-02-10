b423877f54294ec6d58d2ffe0c18a95831e889d9
Aleksandr Dergunov <dergunovs@mail.ru>
2025-02-10 09:52:57 +0300

    test: id and link helpers

---
+++ packages/helpers/src/helpers/id.spec.ts
@@ -0,0 +1,35 @@
+import { describe, expect, test } from 'vitest';
+
+import { createTempId, deleteTempId } from '.';
+
+describe('id', () => {
+  test('creates temp id', async () => {
+    expect(createTempId()).toContain('temp-');
+  });
+
+  test('deletes temp id', async () => {
+    const objectWithTempId = {
+      _id: 'temp-f0e5ba29-1a71-4126-aa9e-86f4e720',
+      title: 'title',
+    };
+
+    const objectWithoutTempId = deleteTempId([objectWithTempId]);
+
+    expect(objectWithoutTempId[0]).toStrictEqual({ title: 'title' });
+  });
+
+  test('deletes all id', async () => {
+    const objectWithId = {
+      _id: 'f0e5ba29-1a71-4126-aa9e-86f4e720',
+      title: 'title',
+    };
+
+    const objectWithoutTempId = deleteTempId([objectWithId]);
+
+    expect(objectWithoutTempId[0]).toStrictEqual(objectWithId);
+
+    const objectWithoutAnyId = deleteTempId([objectWithId], true);
+
+    expect(objectWithoutAnyId[0]).toStrictEqual({ title: 'title' });
+  });
+});
---
+++ packages/helpers/src/helpers/link.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from 'vitest';
+
+import { isLinkActive } from '.';
+
+describe('link', () => {
+  test('checks is link active', async () => {
+    const mainUrl = '/';
+    const catalogUrl = '/product';
+    const productUrl = '/product/123';
+
+    expect(isLinkActive(mainUrl, mainUrl)).toBe(true);
+    expect(isLinkActive(catalogUrl, catalogUrl)).toBe(true);
+    expect(isLinkActive(productUrl, productUrl)).toBe(true);
+    expect(isLinkActive(productUrl, catalogUrl)).toBe(true);
+    expect(isLinkActive(productUrl, mainUrl)).toBe(false);
+    expect(isLinkActive(catalogUrl, productUrl)).toBe(false);
+  });
+});
