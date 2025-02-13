5097c7536416b107e3b7aaeb4f85114186dfe32f
Aleksandr Dergunov <dergunovs@mail.ru>
2025-02-13 19:53:13 +0300

    test: useInfiniteScroll

---
+++ packages/helpers/src/composables/useInfiniteScroll.spec.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from 'vitest';
+
+import { withSetup } from '..';
+import { useInfiniteScroll } from '.';
+
+interface IData {
+  id: number;
+  title: string;
+}
+
+const data: IData[] = [
+  { id: 1, title: 'Первый' },
+  { id: 2, title: 'Второй' },
+];
+
+describe('useInfiniteScroll', () => {
+  test('handles scroll', async () => {
+    withSetup(() => {
+      const { page, allData, addData, handleScroll } = useInfiniteScroll<IData>();
+
+      expect(page.value).toStrictEqual(1);
+      expect(allData.value).toStrictEqual([]);
+
+      addData(data);
+
+      expect(allData.value).toStrictEqual(data);
+
+      addData(data);
+
+      expect(allData.value).toStrictEqual([...data, ...data]);
+
+      const secondPageNumber = 2;
+
+      handleScroll(false, secondPageNumber);
+
+      expect(page.value).toStrictEqual(secondPageNumber);
+    });
+  });
+});
