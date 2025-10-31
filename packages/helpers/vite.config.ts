5219f82ab663b64fcfe6f8e414a699ae14b4427b
Aleksandr Dergunov <dergunovs@mail.ru>
2025-10-31 13:11:26 +0300

    ci: type check

--- packages/helpers/vite.config.ts
+++ packages/helpers/vite.config.ts
@@ -4,7 +4,6 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import vue from '@vitejs/plugin-vue';
 
 const composables = fs
@@ -56,43 +55,9 @@ export default defineConfig({
       entryRoot: './src',
       exclude: ['**/*.spec.ts'],
     }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/index.ts',
-          dest: '',
-          rename: 'index.js',
-          transform: (contents) =>
-            contents
-              .toString()
-              .replace(
-                /export \* from '\.\/composables\/([^']*)';/g,
-                (_match, name) => `export * from './${name}/index.js';`
-              )
-              .replace(
-                /export \* from '\.\/helpers\/([^']*)';/g,
-                (_match, name) => `export * from './${name}/index.js';`
-              ),
-        },
-        {
-          src: 'src/index.ts',
-          dest: '',
-          rename: 'index.d.ts',
-          transform: (contents) => {
-            let str = contents.toString().replace(/.ts/g, '');
-
-            str = str.replace(/export \* from '\.\/composables\/([^']*)';/g, "export * from './$1';");
-            str = str.replace(/export \* from '\.\/helpers\/([^']*)';/g, "export * from './$1';");
-
-            return str;
-          },
-        },
-      ],
-    }),
     {
-      name: 'move-dts',
+      name: 'generate-index-files',
       apply: 'build',
-
       closeBundle() {
         const distDir = path.resolve('dist');
 
@@ -122,6 +87,35 @@ export default defineConfig({
 
         moveDts('composables');
         moveDts('helpers');
+
+        const srcIndexPath = path.resolve('src/index.ts');
+        const distIndexJsPath = path.join(distDir, 'index.js');
+        const distIndexDtsPath = path.join(distDir, 'index.d.ts');
+
+        const contents = fs.readFileSync(srcIndexPath, 'utf8');
+
+        const transformedJsContents = contents
+          .toString()
+          .replace(
+            /export \* from '\.\/composables\/([^']*)';/g,
+            (_match, name) => `export * from './${name}/index.js';`
+          )
+          .replace(/export \* from '\.\/helpers\/([^']*)';/g, (_match, name) => `export * from './${name}/index.js';`);
+
+        fs.writeFileSync(distIndexJsPath, transformedJsContents);
+
+        let transformedDtsContents = contents.toString().replace(/.ts/g, '');
+
+        transformedDtsContents = transformedDtsContents.replace(
+          /export \* from '\.\/composables\/([^']*)';/g,
+          "export * from './$1';"
+        );
+        transformedDtsContents = transformedDtsContents.replace(
+          /export \* from '\.\/helpers\/([^']*)';/g,
+          "export * from './$1';"
+        );
+
+        fs.writeFileSync(distIndexDtsPath, transformedDtsContents);
       },
     },
   ],
